9c6f473b4ae3eb52d44aa85d49025214fdcf4530
André Veiga <a.branco.veiga@gmail.com>
2022-06-14 20:04:07 +0100

    cleaning up the token loading

--- guitars-nodejs.js
+++ guitars-nodejs.js
@@ -1,16 +1,18 @@
 import http from "k6/http";
 import { sleep } from "k6";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 export const options = {
-  vus: 150,
-  duration: "30s",
+  vus: process.env.VU,
+  duration: `${process.env.duration}s`,
 };
 
 export default function () {
   const params = {
     headers: {
-      // "Content-Type": "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJjYTR0d0hwWE1lLXdBdzVSb0FyWWJaUzMwS1o0R0p6bXhWaFQ4azhIY0I0In0.eyJleHAiOjE2NTUyMjQyNDIsImlhdCI6MTY1NTIyMzM0MiwianRpIjoiZjIxNTBhMDMtN2NiMy00ZDFlLWFkYmUtMDgxODcwZDc4ZGE2IiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgwL2F1dGgvcmVhbG1zL21hc3RlciIsImF1ZCI6ImFjY291bnQiLCJzdWIiOiJhOWIyY2UzOS1jNGZmLTQyY2MtOTNmZC0wN2ZhMTc2NjY0NzMiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJteWFwcCIsImFjciI6IjEiLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsiZGVmYXVsdC1yb2xlcy1tYXN0ZXIiLCJvZmZsaW5lX2FjY2VzcyIsInVtYV9hdXRob3JpemF0aW9uIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJwcm9maWxlIGVtYWlsIiwiY2xpZW50SG9zdCI6IjE3Mi4xOC4wLjEiLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImNsaWVudElkIjoibXlhcHAiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJzZXJ2aWNlLWFjY291bnQtbXlhcHAiLCJjbGllbnRBZGRyZXNzIjoiMTcyLjE4LjAuMSJ9.J3lM2UIS4qeeCXV78DBTdPPTox3PcdC7FQWvSXg4W7hxPj1uDRPuQeUjqsFM9vBw5bCRBam0Cn8hRah2BCdPdeC3f9c3CM97wqJlhfvS-03MAUkJNiRt5glTbLpshN3Ve1N0_Xl9QBIf1iq5tXXtvFZLi2ERjCVsaHzI1cPbvc4f5M1SAmAxU7_fmtJJeyEWYTJgjPIdPaK1ev4Amx-pqxI4PRd6Vp0CHNY0ZNHLgiOcJn_a6UFqjNqvTY0nsqMSyvyFqS_4u-8YndP12-BeEUQEnppcvonYSVs3eOEb75al4Alz0MmlwJp9n6CpqbN4ntNSGegiCJREZ4-g-r65RQ",
+      Authorization: `Bearer ${process.env.OAUTH_TOKEN}`,
     },
   };
 
